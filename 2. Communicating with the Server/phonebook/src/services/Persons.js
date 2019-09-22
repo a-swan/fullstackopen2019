02d5730277b9d37517ce3145b36eb4132a2bcb73
alexpswanson <alexpswanson@gmail.com>
2019-09-22 13:55:09 +0300

    update phonebook unfinished ountries

---
+++ 2. Communicating with the Server/phonebook/src/services/Persons.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/Persons'
+
+const getAll = () => {
+    return axios.get(baseUrl)
+}
+
+const create = newObject => {
+    return axios.post(baseUrl, newObject)
+}
+
+const remove = id => {
+    console.log(`${baseUrl}/${id}`)
+    return axios.delete(`${baseUrl}/${id}`)
+}
+
+const update = (id, updatedObject) => {
+    const request = axios.put(`${baseUrl}/${id}`, updatedObject)
+    return request.then(response => response.data)
+}
+
+export default { getAll, create, remove, update }
