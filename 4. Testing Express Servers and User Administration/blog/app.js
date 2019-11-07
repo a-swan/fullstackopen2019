f88f74977155a32ed33bd3eef9c3d12b407ba7a2
alexpswanson <alexpswanson@gmail.com>
2019-11-07 14:59:06 +0200

    add middleware error logger

--- 4. Testing Express Servers and User Administration/blog/app.js
+++ 4. Testing Express Servers and User Administration/blog/app.js
@@ -4,12 +4,12 @@ const bodyParser = require('body-parser')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
-// const middleware = require('./utils/middleware')
+const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 
 console.log('connection to', config.MONGODB_URI)
 
-mongoose.connect(config.MONGODB_URI, {useNewUrlParser: true})
+mongoose.connect(config.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => {
         console.log('connected to MongoDB')
     })
@@ -20,11 +20,11 @@ mongoose.connect(config.MONGODB_URI, {useNewUrlParser: true})
 app.use(cors())
 app.use(express.static('build'))
 app.use(bodyParser.json())
-// app.use(middleware.requestLogger)
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 
-// app.use(middleware.unknownEndpoint)
-// app.use(middleware.errorHandler)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
