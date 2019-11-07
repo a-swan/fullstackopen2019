197ae1868cfc5bdbd933965b0363ca0dc5c6c4cb
alexpswanson <alexpswanson@gmail.com>
2019-11-07 14:52:40 +0200

    refactor index.js

--- 4. Testing Express Servers and User Administration/blog/index.js
+++ 4. Testing Express Servers and User Administration/blog/index.js
@@ -1,52 +1,9 @@
+const app = require('./app.js')
+const http = require('http')
 const config = require('./utils/config')
 
-const http = require('http')
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const mongoose = require('mongoose')
+const server = http.createServer(app)
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = config.MONGODB_URI
-
-mongoose.connect(mongoUrl, { useNewUrlParser: true })
-    .then(result => {
-        console.log("Connected to MongoDB")
-    })
-    .catch(error => {
-        console.log("Error connecting to MongoDB", error.message)
-    })
-
-app.use(cors())
-app.use(bodyParser.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog
-        .save()
-        .then(result => {
-        response.status(201).json(result)
-        })
-})
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
 })
