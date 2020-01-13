83977981780f78712e0267716b6ad392449dca65
alexpswanson <alexpswanson@gmail.com>
2020-01-13 20:31:48 +0200

    4.19 new blogs only created with vaild token

--- 4. Testing Express Servers and User Administration/blog/controllers/blogs.js
+++ 4. Testing Express Servers and User Administration/blog/controllers/blogs.js
@@ -1,6 +1,16 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+
+    if(authorization && authorization.toLowerCase().startsWith('bearer ')){
+        return authorization.substring(7)
+    }
+    return null
+}
 
 blogsRouter.get('/', async (request, response, next) => {
     const blogs = await Blog
@@ -12,21 +22,29 @@ blogsRouter.get('/', async (request, response, next) => {
 blogsRouter.post('/', async (request, response, next) => {
     const body = request.body
 
-    const users = await User.find({})
-    const user = users[0]
+    const token = getTokenFrom(request)
 
-    const blog = new Blog({
-        title: body.title,
-        author: body.author,
-        user: user._id,
-        url: body.url,
-        likes: body.likes === undefined ? 0 : body.likes
-    })
     try{
+        const decodedToken = jwt.verify(token, process.env.SECRET)
+        if(!token || !decodedToken.id){
+            return response.status(401).json({error: "token missing or invalid"})
+        }
+        
+        const user = await User.findById(decodedToken.id)
+
+        const blog = new Blog({
+            title: body.title,
+            author: body.author,
+            user: user._id,
+            url: body.url,
+            likes: body.likes === undefined ? 0 : body.likes
+        })
+
         const savedBlog = await blog.save()
         user.blogs = user.blogs.concat(savedBlog._id)
         await user.save()
         response.status(201).json(savedBlog.toJSON())
+
     } catch(exception){
         next(exception)
     }    
