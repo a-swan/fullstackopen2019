6c679682609ae075ab8aaaea38d6e444b59e7916
alexpswanson <alexpswanson@gmail.com>
2020-01-23 16:18:07 +0200

    blog styles

--- 5. Testing React Apps, Custom Hooks/bloglist-frontend/src/components/Blog.js
+++ 5. Testing React Apps, Custom Hooks/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,19 @@
 import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    border: 'solid',
+    borderWidth: 1,
+    padding: 5,
+    margin: 5
+  }
+
+  return (
+    <div style={blogStyle} onClick={() => console.log('clicked', blog.title)}>
+      {blog.title} {blog.author}
+    </div>
+  )
+}
 
 export default Blog
