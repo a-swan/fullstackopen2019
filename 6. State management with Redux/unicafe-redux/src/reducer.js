30ec03766e5afe984f87e617ed402be09f7e171d
alexpswanson <alexpswanson@gmail.com>
2020-01-29 19:42:34 +0200

    6 init

---
+++ 6. State management with Redux/unicafe-redux/src/reducer.js
@@ -0,0 +1,31 @@
+const initialState = {
+    good: 0,
+    ok: 0,
+    bad: 0
+  }
+  
+  const counterReducer = (state = initialState, action) => {
+    console.log(state, action)
+    switch (action.type) {
+      case 'GOOD':
+        state.good += 1
+        return state
+      case 'OK':
+        state.ok += 1
+        return state 
+      case 'BAD':
+        state.bad += 1
+        return state
+      case 'ZERO':
+        state.good = 0
+        state.ok = 0
+        state.bad = 0
+        return state
+      default:
+          console.log(state, action)
+          return state
+    }
+    return state
+  }
+  
+  export default counterReducer
