0a11088e21ad5f1cdfcf55330600b878c81daab1
alexpswanson <alexpswanson@gmail.com>
2020-01-31 16:16:39 +0200

    6.5 sort by votes

--- 6. State management with Redux/redux-anecdotes/src/components/Anecdotes.js
+++ 6. State management with Redux/redux-anecdotes/src/components/Anecdotes.js
@@ -4,10 +4,13 @@ import Anecdote from './Anecdote'
 import {vote} from '../reducers/anecdoteReducer'
 
 const Anecdotes = ({store}) => {
+    const anecdotes = store.getState().sort((a1, a2) => {
+        return ((a1.votes < a2.votes) ? 1 : (a1.votes > a2.votes) ? -1 : 0)
+    })
     return (
         <>
             <h2>Anecdotes</h2>
-            {store.getState().map(anec => 
+            {anecdotes.map(anec => 
                 <Anecdote
                     key={anec.id}
                     anec={anec}
