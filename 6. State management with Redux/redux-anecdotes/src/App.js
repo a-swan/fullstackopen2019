6e5dfed2f1345bf4b36f2d3bac142c8810ca7399
alexpswanson <alexpswanson@gmail.com>
2020-02-10 21:27:27 +0200

    6.14

--- 6. State management with Redux/redux-anecdotes/src/App.js
+++ 6. State management with Redux/redux-anecdotes/src/App.js
@@ -7,10 +7,10 @@ import Filter from './components/Filter'
 const App = (props) => {
   return (
     <div>
-      <Notification store={props.store}/>
-      <Filter store={props.store}/>
-      <Anecdotes store={props.store}/>
-      <AnecdoteForm store={props.store}/>
+      <Notification />
+      <Filter />
+      <Anecdotes />
+      <AnecdoteForm />
     </div>
   )
 }
